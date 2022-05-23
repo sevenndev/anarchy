0846fe1ea43cd811de82c5f3da0fb80cc716690e
afinity <92194055+AfinityDev@users.noreply.github.com>
2022-05-23 19:01:26 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AfinityDev/anarchy/main/arsenal.lua'))()
+end
+
+if game.PlaceId == 1962086868 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AfinityDev/anarchy/main/toh.lua'))()
+end
