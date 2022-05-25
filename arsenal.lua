397d7272c1e353494a505bf55119b120a2e58bee
afinity <92194055+AfinityDev@users.noreply.github.com>
2022-05-24 20:05:33 -0400

    Update arsenal.lua

--- arsenal.lua
+++ arsenal.lua
@@ -109,6 +109,43 @@ gm.__index = newcclosure(function(self,i)
     return OldIndex(self,i)
 end)
 
+AimbotSection:addButton({
+    title = "Kill All",
+    callback = function()
+        local Gun = game.ReplicatedStorage.Weapons:FindFirstChild(game.Players.LocalPlayer.NRPBS.EquippedTool.Value)
+        local Crit = math.random() > .6 and true or false
+        for i, v in pairs(game.Players:GetPlayers()) do
+            if v and v.Character and v.Character:FindFirstChild("Head") then
+                local Distance = (game.Players.LocalPlayer.Character.Head.Position - v.Character.Head.Position).magnitude
+                for i = 1, 10 do
+                    game.ReplicatedStorage.Events.HitPart:FireServer(
+                        v.Character.Head,
+                        v.Character.Head.Position + Vector3.new(math.random(), math.random(), math.random()),
+                        Gun.Name,
+                        Crit and 2 or 1,
+                        Distance,
+                        Backstab,
+                        Crit,
+                        false,
+                        1,
+                        false,
+                        Gun.FireRate.Value,
+                        Gun.ReloadTime.Value,
+                        Gun.Ammo.Value,
+                        Gun.StoredAmmo.Value,
+                        Gun.Bullets.Value,
+                        Gun.EquipTime.Value,
+                        Gun.RecoilControl.Value,
+                        Gun.Auto.Value,
+                        Gun["Speed%"].Value,
+                        game.ReplicatedStorage.wkspc.DistributedTime.Value
+                    )
+                end
+            end
+        end        
+    end
+})
+
 AimbotSection:addToggle({
     title = "WallBang",
     callback = function(value)
